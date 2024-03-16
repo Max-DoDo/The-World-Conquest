142123349db3019f073ad0862fd364b0dfe459cb
Max <great_maxwell@outlook.com>
2024-03-17 03:31:26 +0800

    fix: Fixed half of the issues at different resolutions
    The rest thing will do in tomorrow. Good night max.
    
    (～﹃～)~zZ

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -1,5 +1,7 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 /// <summary>
@@ -24,7 +26,7 @@ public class Camera_Controller : MonoBehaviour
     ///     Meanwhile, setting the value too large will cause Unity to throw an exception.
     ///     Negative values will result in the map being flipped.
     /// </summary>
-    private float init_CameraSize;
+    private float init_ZoomSize;
 
     /// <summary>
     ///     A interpolate used to modify the distance the camera moves each time. 
@@ -39,14 +41,14 @@ public class Camera_Controller : MonoBehaviour
     ///     note that this value is not the final determinant; 
     ///     it will be interpolated to maintain nearly the same scaling ratio under different scaling sizes.
     /// </summary>
-    public Vector2 minXY;
+    private Vector2 MinXY;
 
     /// <summary>
     ///     The 2D vector contain the maxinum value of camera position in X and Y.
     ///     note that this value is not the final determinant; 
     ///     it will be interpolated to maintain nearly the same scaling ratio under different scaling sizes.
     /// </summary>
-    public Vector2 maxXY;
+    private Vector2 MaxXY;
 
     /// <summary>
     ///     This value control the constant value in the equation for calculate the camera zoom rate.
@@ -79,6 +81,8 @@ public class Camera_Controller : MonoBehaviour
     /// </summary>
     private float minZoomSize;
 
+    public Canvas canvas;
+
     /// <summary>
     ///     The method call by Unity when launch. In this method, every field defined above will be init.
     ///     Meanwhile, Reset the camera size and location in a given value.
@@ -88,19 +92,24 @@ public class Camera_Controller : MonoBehaviour
     /// </returns>
     private void Start()
     {
-        moveSpeed = 50.0f;
-        init_CameraSize = 5f;
-        mouseWheelSpeed = 0.5f;
+
+        RectTransform rt = canvas.GetComponent<RectTransform>();
+        Vector2 screenSize = new(Screen.currentResolution.width,Screen.currentResolution.height);
+        
+        moveSpeed = screenSize.x/5;
+        init_ZoomSize = screenSize.x/10;
+        mouseWheelSpeed = screenSize.x/20;
         adjustment_value = 0.0f;
         offset = 0.0f;
-        maxZoomSize = 8.0f;
-        minZoomSize = 2.0f;
+        maxZoomSize = screenSize.x/5;
+        minZoomSize = screenSize.x/20;
 
-        minXY = new Vector2(-10, -8);
-        maxXY = new Vector2(10, 8);
+        // canvas = GetComponent<Canvas>();
 
         // Application.targetFrameRate = 60;
-        Camera.main.orthographicSize = init_CameraSize;
+        Camera.main.orthographicSize = init_ZoomSize;
+        Camera.main.transform.position = canvas.transform.position;
+
     }
 
     /// <summary>
@@ -111,34 +120,53 @@ public class Camera_Controller : MonoBehaviour
     {
         ZoomCamera();
         MoveCamera();
+        // Debug.Log(Camera.main.transform.position);
+        
     }
 
     private void MoveCamera()
     {
-        adjustment_value = Camera.main.orthographicSize / init_CameraSize * Time.deltaTime * moveSpeed * 2;
+
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0){
+            return;
+        }
+
+        // Get Movement speed and direction
+        adjustment_value = Camera.main.orthographicSize / init_ZoomSize * Time.deltaTime * moveSpeed;
         float horizontalInput = Input.GetAxis("Horizontal") * adjustment_value;
         float verticalInput = Input.GetAxis("Vertical") * adjustment_value;
 
         Vector2 moveAmount = new(horizontalInput, verticalInput);
         Vector2 newPosition = new Vector2(transform.position.x, transform.position.y) + moveAmount;
 
-
+        // Get a value between 0 and 1 for the zoom rate.
         float LerpValue = Mathf.InverseLerp(minZoomSize, maxZoomSize, Camera.main.orthographicSize);
         LerpValue = (float)Math.Round(LerpValue * 100f) / 100;
-        // Debug.Log(LerpValue);
         LerpValue = Mathf.Clamp01(LerpValue);
-        Vector2 newMinXY = Vector2.Lerp(minXY, new Vector2(0, 0), LerpValue);
-        Vector2 newMaxXY = Vector2.Lerp(maxXY, new Vector2(0, 0), LerpValue);
 
-        newPosition.x = Mathf.Clamp(newPosition.x, newMinXY.x, newMaxXY.x);
-        newPosition.y = Mathf.Clamp(newPosition.y, newMinXY.y, newMaxXY.y);
+        float width = Screen.currentResolution.width;
+        float height = Screen.currentResolution.height;
+        float ScreenResolutionRate = width/height;
+        float size = Camera.main.orthographicSize;
 
+        MinXY = new Vector2(size * ScreenResolutionRate + 200,size);
+        MaxXY = new Vector2((width - size * ScreenResolutionRate) - 200,height - size);
+        Debug.Log("Size: " + Camera.main.orthographicSize);
+        Debug.Log("MinXY: " + MinXY);
+        Debug.Log("MaxXY: " + MaxXY);
 
+        // Vector2 newMinXY = MinXY * LerpValue;
+        // Vector2 newMaxXY = MaxXY * LerpValue;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, MinXY.x, MaxXY.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, MinXY.y, MaxXY.y);
+
+        
         transform.position = newPosition;
 
     }
 
-
+//TODO 将屏幕分辨率修改为canvas的长和宽
     private void ZoomCamera()
     {
 
